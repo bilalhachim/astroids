c3316f0a58e935f773d46cf6c493cc04d8171093
user1 <user1@DESKTOP-83IQKLM>
2024-10-02 15:26:11 +0100

    new one

--- main.py
+++ main.py
@@ -3,8 +3,13 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
 def main():
     pygame.init()
+    obj = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
    
     screen.fill((0, 0, 0), rect=None, special_flags=0) 
@@ -13,10 +18,9 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return    
-            
-
-
+            dt = dt - obj.tick(60)       
+        player.draw(screen)
 
 
 if __name__ == "__main__":
-      main()    
+      main()  s
