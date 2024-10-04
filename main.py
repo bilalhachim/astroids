bce0a7e8172a332b79e6f88e9dbad26127421397
bilalhachim <bilal.hachim.off@gmail.com>
2024-10-04 08:18:42 +0100

    third_one

--- main.py
+++ main.py
@@ -1,26 +1,40 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
 from constants import *
-from player import *
+from player import Player
+from asteroid  import *
+from asteroidfield import * 
+
+
 def main():
     pygame.init()
-    obj = pygame.time.Clock()
-    dt = 0
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-   
-    screen.fill((0, 0, 0), rect=None, special_flags=0) 
-    pygame.display.flip()
-    while(True):
+    clock = pygame.time.Clock()
+    dt = 0
+    asteroids = pygame.sprite.Group()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    AsteroidField.containers = (updatable)
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroidfield = AsteroidField()
+
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                return    
-            dt = dt - obj.tick(60)       
-        player.draw(screen)
+                return
+
+        screen.fill("black")
+        for thing in updatable:
+            thing.update(dt)    
+        for thing in drawable:
+            thing.draw(screen)
+
+        pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
-      main()  s
+    main()
